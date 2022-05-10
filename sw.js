2966247a4330dd50e45e5f77fb0a8591a99094d4
Trav <tjerome@armadillo.local>
2022-05-10 11:29:27 +0100

    1
    1

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220412.1842';
+const cacheName = 'chirpy-20220510.1128';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
