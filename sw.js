1a01f7ff639b9d1a11e398e036c4ab95c9df150f
Travis Jerome <tjerome@armadillo.local>
2022-09-04 12:04:00 +0100

    update
    uodate

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220716.1825';
+const cacheName = 'chirpy-20220904.1202';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
